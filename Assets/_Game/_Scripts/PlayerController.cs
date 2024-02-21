544b3e00c8144ae4c6f47233f80e73b1b27bf5e4
LeandroRCorreia <leandrohamlet@gmail.com>
2024-02-21 15:39:45 -0300

    New input system suports Keyboard and virtual JoySticks

--- Assets/_Game/_Scripts/PlayerController.cs
+++ Assets/_Game/_Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterMovement3D))]
 public class PlayerController : MonoBehaviour
 {
 
     private CharacterMovement3D characterMovement3D;
+
+    private InputActions playerInputs;
 
     public static PlayerController PlayerInstance {get; private set;} 
 
@@ -19,17 +23,31 @@ public class PlayerController : MonoBehaviour
             Destroy(this);
         }
         characterMovement3D = GetComponent<CharacterMovement3D>();
+        playerInputs = new();
 
     }
 
+    void Start()
+    {
+        playerInputs.Enable();
+        playerInputs.Player.Attack.performed += OnPlayerAttack;
+    }
+
+
+
     void Update()
     {
-        var horizontal = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Vertical");
+        Vector2 input = playerInputs.Player.Movement.ReadValue<Vector2>();
 
 
 
-        characterMovement3D.SetInput(horizontal, vertical);
+
+        characterMovement3D.SetInput(input.x, input.y);
+    }
+
+    private void OnPlayerAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attacking!");
     }
 
 }
